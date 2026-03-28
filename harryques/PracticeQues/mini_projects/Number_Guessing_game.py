828d216454f06d98f9020f6e31a25ba32e133c9f
Sneha <snehasaini143786@gmail.com>
2026-03-28 23:50:24 +0530

    commit changes

--- harryques/PracticeQues/mini_projects/Number_Guessing_game.py
+++ harryques/PracticeQues/mini_projects/Number_Guessing_game.py
@@ -12,6 +12,22 @@ num=random.randint(1,10)
 print(num)
 tries=0
 
+while True:
+    guess=int(input("Guess  your number between 1 to 10: "))
+
+    if num==guess :
+       tries+=1
+       print(f"Right guess,the number is {tries} tries")
+       break
+    elif num < guess :
+         print("Go lower")
+    elif num>guess :
+        print("Go higher")
+        tries+=1
+    else:
+        tries+=1
+        print("Sorry you are wrong")         
+
 
 
  
